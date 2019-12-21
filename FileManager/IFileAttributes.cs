0de4f06443a3fb55914eb4f37d552fea504dd239
Dani <danirdd92@gmail.com>
2019-12-21 19:47:57 +0200

    Created File Manager Project and made Abstract class MyFile, Extracted IFileAttributes Interface

---
+++ FileManager/IFileAttributes.cs
@@ -0,0 +1,11 @@
+﻿namespace FileManager
+{
+    public interface IFileAttributes
+    {
+        string FilePath { get; }
+        int FileSize { get; }
+        bool IsArchived { get; }
+        bool IsInfected { get; }
+        bool IsReadOnly { get; }
+    }
+}
---
+++ FileManager/MyFile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileManager
+{
+    public abstract class MyFile : IFileAttributes
+    {
+        const bool ISHEBSUPPORTED = true;
+        private readonly string _filePath;
+        private static List<string> _paths;
+
+        public string FilePath
+        {
+            get
+            {
+                return _filePath;
+            }
+        }
+
+        public int FileSize { get; private set; }
+
+        public bool IsReadOnly { get; private set; }
+
+        public bool IsArchived { get; private set; }
+
+        public bool IsInfected { get; private set; }
+
+        public MyFile(string path)
+        {
+            _filePath = path;
+        }
+
+
+        public abstract void PrintFiie();
+
+
+    }
+}
